4b7e7d6a541bc4048f636390c68e9f452aa41958
Jeremy Vorrink <jeremyvorrink@hotmail.com>
2021-09-10 13:45:02 +0200

    Add files via upload

---
+++ Jaar2021Opdracht2.ino
@@ -0,0 +1,18 @@
+//Instantieer en definier de poorten
+const int ledPin = 11;
+const int buttonPin = 2;
+      bool lastStatus = false;  //Hou de status bij van de LED
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT); //pinMode instellen zodat we ze kunnen gebruiken
+  pinMode(buttonPin, INPUT); //ledPin is OUTPUT want we willen ertegen praten, buttonPin is INPUT want we willen wat hij te zeggen heeft
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(buttonPin) == HIGH){ //Als de knop word ingedrukt gaan we de if statement in
+    lastStatus = !lastStatus; //We draaien de boolean om door te zeggen dat de boolean het tegenovergestelde is van zijn huidige waarde
+    digitalWrite(ledPin, lastStatus); //We geven deze waarde door aan de led
+    delay(1000); //We laten de arduino even wachten zodat het lampje niet héél snel aan en uit gaat
+  }
+}
